db0e89e57150f4b7346c17d26f5416c6c149531d
aleasegur <aleasegur@alu.edu.gva.es>
2026-01-13 10:30:07 +0100

    add Button.vue

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,9 +1,15 @@
 <script setup>
-
+import { ref } from 'vue'
+defineProps({
+  type: String,
+  clase: String,
+})
 </script>
 
 <template>
-
+<div>
+  <button :type="type" :clase="clase"></button>
+</div>
 </template>
 
 <style scoped>
