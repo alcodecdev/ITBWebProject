9617d591ea9f7b6fa708ab796aa64c325630634a
aleja <aasenciogurau@gmail.com>
2026-01-13 17:11:52 +0100

    add nuevas plantillas Formulario y Scanner

---
+++ src/views/ScannerView.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+import Navbar from "@/components/layout/Navbar.vue";
+import Button from "@/components/Button.vue";
+import router from "@/router/index.js";
+
+const goToForm = () => {
+  router.push('/form')
+}
+
+</script>
+
+<template>
+<div class="bg-light text-white min-vh-100">
+
+  <Navbar />
+
+
+  <div class="container text-center">
+
+
+
+
+
+
+    <div class="fixed-bottom p-3 bg-light bg-opacity-90">
+      <div class="container d-grid">
+        <Button
+            @click="goToForm"
+            nombreSpan="FINALIZAR Y ENVIAR FORMULARIO"
+            clase="btn btn-success btn-lg fw-bold py-3 shadow"/>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
