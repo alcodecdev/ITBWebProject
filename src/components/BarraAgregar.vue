42cef78cf8142cb40b39787b066c8df4a4abde5e
aleasegur <aleasegur@alu.edu.gva.es>
2026-01-14 09:00:49 +0100

    add nuecos componentes

---
+++ src/components/BarraAgregar.vue
@@ -0,0 +1,61 @@
+<script setup>
+defineProps({
+  divClassPadre:{
+    type:String,
+    default: "row justify-content-center mb-4"
+  },
+  divClassHijo:{
+    type:String,
+    default: "col-12 col-md-8 col-lg-5"
+  },
+  divClassNieto:{
+    type:String,
+    default: "input-group input-group-lg"
+  },
+  divClassParrafo:{
+    type:String,
+    default: "text-danger fw-bold mt-2 small"
+  },
+  inputType:{
+    type:String
+  },
+  inputClass:{
+    type:String,
+    default: "form-control bg-light text-dark border-secondary focus-ring focus-ring-light"
+  },
+  inputPlaceholder:{
+    type:String,
+    default:"Codigo o FR..."
+  },
+  buttonClass:{
+    type:String,
+    default:"btn btn-primary fw-bold px-4"
+  },
+  buttonName:{
+    type:String,
+    default:"Agregar",
+  }
+
+})
+</script>
+
+<template>
+  <div :class="divClassPadre">
+    <div :class="divClassHijo">
+      <div :class="divClassNieto">
+        <input :type="inputType"
+               :class="inputClass"
+               :placeholder="inputPlaceholder"
+               autofocus>
+        <button :class="buttonClass">
+          {{ buttonName }}
+        </button>
+      </div>
+      <p></p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/SpinnerAnimation.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps({
+  divClasePadre:{
+    type: String,
+    default: 'my-5 position-relative d-flex justify-content-center align-items-center'
+  },
+  styleClasePadre:{
+    type: String,
+    default: 'height: 120px;'
+  },
+  divClaseHijoUno:{
+    type: String,
+    default: 'spinner-grow text-info position-absolute'
+  },
+  divClaseStyleUno:{
+    type: String,
+    default: 'width: 100px; height: 100px; opacity: 0.2;'
+  },
+  divClaseHijoDos:{
+    type: String,
+    default: 'spinner-border text-info'
+  },
+  divClaseStyleDos:{
+    type: String,
+    default: 'width: 50px; height: 50px; border-width: 3px;'
+  }
+})
+</script>
+
+<template>
+  <div :class="divClasePadre" :style="styleClasePadre">
+    <div :class="divClaseHijoUno" :style="divClaseStyleUno"></div>
+    <div :class="divClaseHijoDos" :style="divClaseStyleDos"></div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
