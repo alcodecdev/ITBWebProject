18a626f624f28433746bba0a13eead4598dd65b8
Pau Albarracín Granell <tacauzumaki837@gmail.com>
2026-01-14 09:43:50 +0100

    recemos

---
+++ src/views/PorciForm.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import TitleAndSubtitle from "@/components/TitleAndSubtitle.vue";
+</script>
+
+<template>
+  <div class="container">
+    <div class="roww justify-content-center">
+      <div class="col-12 col-lg-10">
+        <title-and-subtitle
+            title="Registre de tramesa"
+            title-class="display-5 fw-bolder text-success"
+            subtitle="Porcí - Control de lots"
+            subtitle-class="text-success text-uppercase small mb-0"
+            divClass="mb-4 border-bottom border-secondary border-opacity-50 pb-3"
+        ></title-and-subtitle>
+      </div>
+    </div>
+  </div>
+
+
+
+</template>
+
+<style scoped>
+
+</style>
