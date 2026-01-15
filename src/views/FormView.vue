2fd11f9541ccc30e715c9097ce9e3181672d0797
Pau Albarracín Granell <tacauzumaki837@gmail.com>
2026-01-15 08:56:56 +0100

    recemos

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -32,7 +32,7 @@ import Button from "@/components/Button.vue";
         ></campo-formulario>
 
         <campo-formulario
-            type="text"
+            type="password"
             label="CONTRASENYA MOVILITAT:"
             placeholder="12345678"
             label-class="form-label fw-bold text-light  small"
@@ -91,18 +91,22 @@ import Button from "@/components/Button.vue";
             divClass="col-md-6"
         ></campo-formulario>
 
-        <campo-formulario
-            type="text"
-            label="CODI CATEGORIA:"
-            placeholder="12345678"
-            label-class="form-label fw-bold text-light  small"
-            input-class="form-control bg-light text-dark border-secondary"
-            div-error-class="text-danger small fw-bold"
-            divClass="col-md-6"
-        ></campo-formulario>
+        <div class="col-md-3">
+          <label class="form-label fw-bold text-light  small" for="inputCategoria">CODI CATEGORIA:</label>
+          <select class="form-select bg-light text-dark border-secondary" id="inputCategoria" required>
+            <option selected disabled value="">Selecciona uno</option>
+            <option value="Engreix">Engreix</option>
+            <option value="Garrins">Garrins</option>
+            <option value="Recria/Transicio">Recria/Transicio</option>
+            <option value="Femelles reproductores">Femelles reproductores</option>
+            <option value="Reposicio">Reposicio</option>
+            <option value="Sementals">Sementals</option>
+          </select>
+          <div id="errorCategoria" class="text-danger small fw-bold"></div>
+        </div>
 
         <campo-formulario
-            type="text"
+            type="date"
             label="DATA SORTIDA:"
             placeholder="12345678"
             label-class="form-label fw-bold text-light  small"
@@ -113,7 +117,7 @@ import Button from "@/components/Button.vue";
         ></campo-formulario>
 
         <campo-formulario
-            type="text"
+            type="date"
             label="DATA ARRIBADA:"
             placeholder="12345678"
             label-class="form-label fw-bold text-light  small"
@@ -122,15 +126,15 @@ import Button from "@/components/Button.vue";
             divClass="col-md-6"
         ></campo-formulario>
 
-        <campo-formulario
-            type="text"
-            label="MOBILITAT (SI/NO)"
-            placeholder="12345678"
-            label-class="form-label fw-bold text-light  small"
-            input-class="form-control bg-light text-dark border-secondary"
-            div-error-class="text-danger small fw-bold"
-            divClass="col-md-6"
-        ></campo-formulario>
+        <div class="col-md-6">
+          <label class="form-label fw-bold text-light  small d-block">MOBILITAT (SI/NO)</label>
+          <div class="btn-group w-100" role="group">
+            <input type="radio" class="btn-check" name="mobilitat" id="siMobilitat" value="si" required>
+            <label class="btn btn-outline-light fw-bold" for="siMobilitat">SÍ</label>
+            <input type="radio" class="btn-check" name="mobilitat" id="noMobilitat" value="no" checked required>
+            <label class="btn btn-outline-light fw-bold" for="noMobilitat">NO</label>
+          </div>
+        </div>
 
         <div class="col-12">
           <h5 class="mt-4 text-light  fw-bold border-top border-secondary border-opacity-25 pt-3">Dades del Transport</h5>
@@ -146,15 +150,18 @@ import Button from "@/components/Button.vue";
             divClass="col-md-6"
         ></campo-formulario>
 
-        <campo-formulario
-            type="text"
-            label="MITJÀ DE TRANSPORT"
-            placeholder="12345678"
-            label-class="form-label fw-bold text-light  small"
-            input-class="form-control bg-light text-dark border-secondary"
-            div-error-class="text-danger small fw-bold"
-            divClass="col-md-6"
-        ></campo-formulario>
+        <div class="col-md-6">
+          <label class="form-label fw-bold text-light  small">MITJÀ DE TRANSPORT:</label>
+          <select class="form-select bg-light text-dark border-secondary" id="selectMedioTransporte" required>
+            <option selected disabled value="">Selecciona uno</option>
+            <option value="Camion">Camión</option>
+            <option value="Avion">Avion</option>
+            <option value="Tren">Tren</option>
+            <option value="Conduccion a pie">Conduccion a pie</option>
+            <option value="Barco">Barco</option>
+            <option value="Otros">Otros</option>
+          </select>
+        </div>
 
 
         <campo-formulario
@@ -188,7 +195,7 @@ import Button from "@/components/Button.vue";
         ></campo-formulario>
 
         <campo-formulario
-            type="text"
+            type="number"
             label="SIRCODE:"
             placeholder="12345678"
             label-class="form-label fw-bold text-light  small"
