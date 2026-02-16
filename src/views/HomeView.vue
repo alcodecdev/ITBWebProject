f5da9b2259dcf59f615a21a852c73783c5e4b889
aleasegur <aleasegur@alu.edu.gva.es>
2026-02-16 10:34:19 +0100

    add id para clase

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup>
-import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import TitleAndSubtitle from "@/components/TitleAndSubtitle.vue";
 import Button from "@/components/Button.vue";
 import Footer from "@/components/layout/Footer.vue";
 import '../assets/styles/coloursAndAnimation.css'
-import Cookies from 'js-cookie';
 
 const router = useRouter()
 
@@ -37,6 +35,7 @@ const goToForm = () => {
               <div class="col-12 mb-5">
                 <Button
                     @click="goToForm"
+                    id="buttonHome"
                     clase="btn btn-success text-white btn-lg py-5 border-0 shadow d-flex flex-column align-items-center justify-content-center btn-main w-100"
                     claseSpan="display-6 fw-bold text-uppercase"
                     nombreSpan="Enviar alta"
@@ -46,6 +45,7 @@ const goToForm = () => {
               <div class="col-12">
                 <Button
                     @click="goToList"
+                    id="buttonHome"
                     clase="btn btn-success text-white btn-lg py-5 border-0 shadow d-flex flex-column align-items-center justify-content-center btn-list w-100"
                     claseSpan="display-6 fw-bold text-uppercase"
                     nombreSpan="Lista de altas"
